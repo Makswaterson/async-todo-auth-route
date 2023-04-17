47c6052df4b5c7db47cd6be27613737b9a9fbb02
Maksym Lukianenko <maks.lukyan@gmail.com>
2023-04-17 12:21:40 +0300

    fix hook

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,8 @@
+import { useSelector } from 'react-redux';
+import {
+  selectUser,
+  selectIsLoggedIn,
+  selectIsRefreshing,
+} from 'redux/auth/selectors';
+
+export const useAuth = () => {};
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,39 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { tasksReducer } from './tasks/slice';
-import { filtersReducer } from './filtersSlice';
+import { authReducer } from './auth/slice';
+
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+];
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
 
 export const store = configureStore({
   reducer: {
+    auth: persistReducer(authPersistConfig, authReducer),
     tasks: tasksReducer,
-    filters: filtersReducer,
   },
+  middleware,
+  devTools: process.env.NODE_ENV === 'development',
 });
+
+export const persistor = persistStore(store);
